dba7e24c12212e7c1bbde5caf8daed9e2797fbd8
ogulcan <ogulcanbicer@gmail.com>
2023-06-09 00:28:56 +0300

    fix

--- lib/ui/fragments/finance/fragment_finance.dart
+++ lib/ui/fragments/finance/fragment_finance.dart
@@ -20,6 +20,9 @@ class _FragmentFinanceState extends State<FragmentFinance> with AutomaticKeepAli
   @override
   bool get wantKeepAlive => true;
   final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey = GlobalKey<RefreshIndicatorState>();
+  bool textScanning = false;
+  XFile? imageFile;
+  String scannedText = "";
 
   @override
   Widget build(BuildContext context) {
@@ -94,129 +97,99 @@ class _FragmentFinanceState extends State<FragmentFinance> with AutomaticKeepAli
   }
 
   Widget _getBody(BuildContext context, ViewModelFragmentFinance viewModel) {
-    return NestedScrollView(
-      controller: viewModel.scrollController,
-      headerSliverBuilder: (context, innerBoxIsScrolled) => headerSliverBuilder(context, viewModel),
-      body: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  bool textScanning = false;
-
-  XFile? imageFile;
-
-  String scannedText = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("Text Recognition example"),
-      ),
-      body: Center(
-          child: SingleChildScrollView(
+    return Center(
+      child: SingleChildScrollView(
         child: Container(
-            margin: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                if (textScanning) const CircularProgressIndicator(),
-                if (!textScanning && imageFile == null)
-                  Container(
-                    width: 300,
-                    height: 300,
-                    color: Colors.grey[300]!,
-                  ),
-                if (imageFile != null) Image.file(File(imageFile!.path)),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        padding: const EdgeInsets.only(top: 10),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.grey,
-                            shadowColor: Colors.grey[400],
-                            elevation: 10,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                          ),
-                          onPressed: () {
-                            getImage(ImageSource.gallery);
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(
-                                  Icons.image,
-                                  size: 30,
-                                ),
-                                Text(
-                                  "Gallery",
-                                  style: TextStyle(fontSize: 13, color: Colors.grey[600]),
-                                )
-                              ],
-                            ),
-                          ),
-                        )),
-                    Container(
-                        margin: const EdgeInsets.symmetric(horizontal: 5),
-                        padding: const EdgeInsets.only(top: 10),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            primary: Colors.white,
-                            onPrimary: Colors.grey,
-                            shadowColor: Colors.grey[400],
-                            elevation: 10,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-                          ),
-                          onPressed: () {
-                            getImage(ImageSource.camera);
-                          },
-                          child: Container(
-                            margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(
-                                  Icons.camera_alt,
-                                  size: 30,
-                                ),
-                                Text(
-                                  "Camera",
-                                  style: TextStyle(fontSize: 13, color: Colors.grey[600]),
-                                )
-                              ],
-                            ),
-                          ),
-                        )),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
+          margin: const EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (textScanning) const CircularProgressIndicator(),
+              if (!textScanning && imageFile == null)
                 Container(
-                  child: Text(
-                    scannedText,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                )
-              ],
-            )),
-      )),
+                  width: 300,
+                  height: 300,
+                  color: Colors.grey[300]!,
+                ),
+              if (imageFile != null) Image.file(File(imageFile!.path)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      padding: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.grey,
+                          shadowColor: Colors.grey[400],
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                        ),
+                        onPressed: () {
+                          getImage(ImageSource.gallery);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.image,
+                                size: 30,
+                              ),
+                              Text(
+                                "Gallery",
+                                style: TextStyle(fontSize: 13, color: Colors.grey[600]),
+                              )
+                            ],
+                          ),
+                        ),
+                      )),
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5),
+                      padding: const EdgeInsets.only(top: 10),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.white,
+                          onPrimary: Colors.grey,
+                          shadowColor: Colors.grey[400],
+                          elevation: 10,
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+                        ),
+                        onPressed: () {
+                          getImage(ImageSource.camera);
+                        },
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(
+                                Icons.camera_alt,
+                                size: 30,
+                              ),
+                              Text(
+                                "Camera",
+                                style: TextStyle(fontSize: 13, color: Colors.grey[600]),
+                              )
+                            ],
+                          ),
+                        ),
+                      )),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                scannedText,
+                style: const TextStyle(fontSize: 20),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 
@@ -239,8 +212,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void getRecognisedText(XFile image) async {
     final inputImage = InputImage.fromFilePath(image.path);
-    final textDetector = GoogleMlKit.vision.textDetector();
-    RecognisedText recognisedText = await textDetector.processImage(inputImage);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    var recognisedText = await textDetector.processImage(inputImage);
     await textDetector.close();
     scannedText = "";
     for (TextBlock block in recognisedText.blocks) {
@@ -250,10 +223,5 @@ class _MyHomePageState extends State<MyHomePage> {
     }
     textScanning = false;
     setState(() {});
-  }
-
-  @override
-  void initState() {
-    super.initState();
   }
 }
